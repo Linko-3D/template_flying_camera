dae592d4b0d7f73923aff947d83d8327ec29c992
Linko-3D <dbekhouc@gmail.com>
2022-02-01 23:51:26 +0100

    tweak

--- flying_camera_3d.gd
+++ flying_camera_3d.gd
@@ -2,16 +2,19 @@ extends Camera3D
 
 var speed_multiplier = 1
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 func _input(event):
-	if event is InputEventMouseMotion:
-		rotation.y -= deg2rad(event.relative.x) / 18
-		rotation.x -= deg2rad(event.relative.y) / 18
-		rotation.x = clamp( rotation.x, deg2rad(-90), deg2rad(90) )
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		if event is InputEventMouseMotion:
+			rotation.y -= event.relative.x / 250
+			rotation.x -= event.relative.y / 250
+			rotation.x = clamp( rotation.x, deg2rad(-90), deg2rad(90) )
 
 func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
 	if Input.is_physical_key_pressed(KEY_SHIFT):
 		speed_multiplier = 2.5
 	else:
@@ -21,4 +24,4 @@ func _process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	position += direction / 10 * speed_multiplier
-	position.y += ( float(Input.is_key_pressed(KEY_SPACE)) - float(Input.is_key_pressed(KEY_CTRL)) )  / 10 * speed_multiplier
+	position.y += ( float(Input.is_key_pressed(KEY_E)) - float(Input.is_key_pressed(KEY_Q)) )  / 10 * speed_multiplier
